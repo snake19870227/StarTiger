bbdddf4f2e5d7ea783819a56a692f7f73a7da4c3
buhuayang <leno_1314@qq.com>
2019-12-25 18:32:13 +0800

    restapi

--- StarTiger-admin/stiger-admin-api/src/main/java/com/snake19870227/stiger/admin/api/controller/MainController.java
+++ StarTiger-admin/stiger-admin-api/src/main/java/com/snake19870227/stiger/admin/api/controller/MainController.java
@@ -1,9 +1,21 @@
 package com.snake19870227.stiger.admin.api.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.KeyPair;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.UUID;
 
 import static com.snake19870227.stiger.admin.api.config.SecurityConfig.*;
 
@@ -22,6 +34,28 @@ public class MainController {
     @PostMapping(path = LOGIN_SUCCESS_PATH)
     public Object loginSuccess() {
         return HttpStatus.OK.value();
+    }
+
+    public static void main(String[] args) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+
+        String token = Jwts.builder().setIssuer("snake")
+                .setIssuedAt(Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()))
+                .setSubject("xixi")
+                .setAudience("xixi")
+                .setExpiration(Date.from(localDateTime.plusMinutes(30).atZone(ZoneId.systemDefault()).toInstant()))
+                .setId(UUID.randomUUID().toString())
+                .signWith(keyPair.getPrivate())
+                .compact();
+
+        System.out.println(token);
+
+        Jws<Claims> jws = Jwts.parser().setSigningKey(keyPair.getPublic()).parseClaimsJws(token);
+
+        System.out.println(jws.toString());
     }
 
 }
