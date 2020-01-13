071539ec2e5e745b92a2dd326e3307dbb3cf0aec
buhuayang <leno_1314@qq.com>
2020-01-13 17:21:28 +0800

    init

--- StarTiger-admin/stiger-admin-common/src/main/resources/sql/ddl.sql
+++ StarTiger-admin/stiger-admin-common/src/main/resources/sql/ddl.sql
@@ -1,7 +1,24 @@
+drop table if exists sys_cfg;
+create table sys_cfg
+(
+    cfg_code  varchar(50) not null,
+    cfg_value varchar(4000),
+    primary key (cfg_code)
+);
+
+drop table if exists sys_org;
+create table sys_org
+(
+    org_id   varchar(32) not null,
+    org_code varchar(50),
+    org_name varchar(500),
+    primary key (org_id)
+);
+
 drop table if exists sys_user;
 create table sys_user
 (
-    user_id         varchar(50) not null,
+    user_id         varchar(32) not null,
     username        varchar(50),
     encode_password varchar(500),
     primary key (user_id)
@@ -10,7 +27,7 @@ create table sys_user
 drop table if exists sys_role;
 create table sys_role
 (
-    role_id   varchar(50),
+    role_id   varchar(32) not null,
     role_code varchar(50),
     role_name varchar(50),
     primary key (role_id)
@@ -19,9 +36,9 @@ create table sys_role
 drop table if exists sys_user_role;
 create table sys_user_role
 (
-    user_role_id varchar(50) not null,
-    user_id      varchar(50),
-    role_id      varchar(50),
+    user_role_id varchar(32) not null,
+    user_id      varchar(32),
+    role_id      varchar(32),
     role_code    varchar(50),
     primary key (user_role_id)
 );
@@ -29,7 +46,7 @@ create table sys_user_role
 drop table if exists sys_resource;
 create table sys_resource
 (
-    res_id   varchar(50),
+    res_id   varchar(32) not null,
     res_code varchar(50),
     res_name varchar(50),
     res_path varchar(500),
@@ -39,18 +56,10 @@ create table sys_resource
 drop table if exists sys_role_resource;
 create table sys_role_resource
 (
-    role_res_id varchar(50) not null,
-    role_id     varchar(50),
+    role_res_id varchar(32) not null,
+    role_id     varchar(32),
     role_code   varchar(50),
-    res_id      varchar(50),
+    res_id      varchar(32),
     res_code    varchar(50),
     primary key (role_res_id)
-);
-
-drop table if exists sys_cfg;
-create table sys_cfg
-(
-    cfg_code  varchar(50) not null,
-    cfg_value varchar(4000),
-    primary key (cfg_code)
 );
--- StarTiger-admin/stiger-admin-common/src/main/resources/sql/dml.sql
+++ StarTiger-admin/stiger-admin-common/src/main/resources/sql/dml.sql
@@ -10,10 +10,6 @@ values ('UR2', 'snake', 'Admin', 'ROLE_Admin');
 insert into sys_user_role (user_role_id, user_id, role_id, role_code)
 values ('UR3', 'admin', 'Admin2', 'ROLE_Admin2');
 
---insert into sys_resource (res_id, res_code, res_name, res_path)
---values ('index', 'index', '入口', '/');
---insert into sys_resource (res_id, res_code, res_name, res_path)
---values ('main', 'main', '主页', '/main');
 insert into sys_resource (res_id, res_code, res_name, res_path)
 values ('sayHello', 'sayHello', '打招呼', '/sayHello');
 insert into sys_resource (res_id, res_code, res_name, res_path)
@@ -23,10 +19,6 @@ values ('RS2', 'RS2', '资源2', '/res2');
 insert into sys_resource (res_id, res_code, res_name, res_path)
 values ('RS3', 'RS3', '资源3', '/res3');
 
---insert into sys_role_resource (role_res_id, role_id, role_code, res_id, res_code)
---values ('RR4', 'R1', 'ROLE_Admin', 'index', 'index');
---insert into sys_role_resource (role_res_id, role_id, role_code, res_id, res_code)
---values ('RR5', 'R1', 'ROLE_Admin', 'main', 'main');
 insert into sys_role_resource (role_res_id, role_id, role_code, res_id, res_code)
 values ('RR6', 'Admin', 'ROLE_Admin', 'sayHello', 'sayHello');
 insert into sys_role_resource (role_res_id, role_id, role_code, res_id, res_code)
