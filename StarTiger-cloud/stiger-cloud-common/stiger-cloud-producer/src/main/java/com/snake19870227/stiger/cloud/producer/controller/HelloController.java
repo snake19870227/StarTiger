8a08ebf5fbb9818082aa7c4b435cca08ffb844b3
buhuayang <leno_1314@qq.com>
2020-01-21 11:19:19 +0800

    cloud

--- StarTiger-cloud/stiger-cloud-common/stiger-cloud-producer/src/main/java/com/snake19870227/stiger/cloud/producer/controller/HelloController.java
+++ StarTiger-cloud/stiger-cloud-common/stiger-cloud-producer/src/main/java/com/snake19870227/stiger/cloud/producer/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.snake19870227.stiger.cloud.producer.controller;
 import com.snake19870227.stiger.context.StarTigerContext;
 import com.snake19870227.stiger.http.RestResponse;
 import com.snake19870227.stiger.http.RestResponseBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +16,16 @@ import java.util.Map;
  * @author Bu HuaYang
  */
 @RestController
+@RefreshScope
 public class HelloController {
+
+    @Value("${stiger.env:noenv}")
+    private String env;
 
     @GetMapping(path = "/hello")
     public RestResponse.DefaultRestResponse hello() {
         Map<String, String> dataMap = new HashMap<>(2);
-        dataMap.put("title", "Hello StarTiger Cloud.");
+        dataMap.put("title", "Hello StarTiger Cloud [" + env + "].");
         dataMap.put("content", "Profiles " + Arrays.toString(StarTigerContext.getActiveProfiles()));
         return RestResponseBuilder.createSuccessRestResp(dataMap);
     }
