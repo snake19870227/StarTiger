804d430c41d69804f20a8c5fbf9e3f487d31bb09
buhuayang <leno_1314@qq.com>
2020-02-03 17:04:06 +0800

    cloud

---
+++ StarTiger-cloud/stiger-cloud-eureka/stiger-cloud-eureka-gateway-spring/src/main/java/com/snake19870227/stiger/cloud/gateway/StarTigerCloudGatewaySpringApplication.java
@@ -0,0 +1,15 @@
+package com.snake19870227.stiger.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bu HuaYang
+ */
+@SpringBootApplication
+public class StarTigerCloudGatewaySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarTigerCloudGatewaySpringApplication.class, args);
+    }
+}
