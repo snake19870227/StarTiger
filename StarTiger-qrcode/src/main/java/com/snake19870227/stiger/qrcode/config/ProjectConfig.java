48c9873717f2551af2ebbe804ee97848427fb34d
buhy <leno_1314@qq.com>
2020-03-23 23:59:12 +0800

    fix: 二维码demo适配脚手架调整

--- StarTiger-qrcode/src/main/java/com/snake19870227/stiger/qrcode/config/ProjectConfig.java
+++ StarTiger-qrcode/src/main/java/com/snake19870227/stiger/qrcode/config/ProjectConfig.java
@@ -1,8 +1,10 @@
 package com.snake19870227.stiger.qrcode.config;
 
-import com.snake19870227.stiger.web.exception.GlobalHandlerExceptionResolver;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.snake19870227.stiger.web.exception.GlobalHandlerExceptionResolver;
+import com.snake19870227.stiger.web.exception.PostWebErrorHandler;
 
 /**
  * @author Bu HuaYang (buhuayang1987@foxmail.com)
@@ -12,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class ProjectConfig {
 
     @Bean
-    public GlobalHandlerExceptionResolver globalHandlerExceptionResolver() {
-        return new GlobalHandlerExceptionResolver();
+    public GlobalHandlerExceptionResolver globalHandlerExceptionResolver(ObjectProvider<PostWebErrorHandler> postExceptionHandlerProvider) {
+        return new GlobalHandlerExceptionResolver(postExceptionHandlerProvider);
     }
 }
